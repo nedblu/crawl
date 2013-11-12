44bb811331ba020c02d7bf51bf48ef0547db2291
Emilio Alberto Ojeda Mendoza <mdos.emil@gmail.com>
2013-11-12 00:13:13 -0600

    [Diseño] Secciones de Administracion del CMS...
    Solo es interfaz en CSS...

---
+++ public/js/template.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	var w = $(document).width();
+	var h = $(document).height();
+	$('#menu').css('height', h);
+});
+
+// $(window).resize(function() {
+//     var bodyheight = $(document).height();
+//     $("#menu").height(bodyheight);
+// });
