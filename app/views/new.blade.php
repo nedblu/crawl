5e0c13abcc10a8dd9dea48087a84b6a796972c10
Felipe Arturo Perez Camargo <argosh725@gmail.com>
2013-12-04 02:32:11 -0600

    Modificación en la migración de users agregado campo nuevo *image* y obtención de la imagen de perfil con DB en templates.

---
+++ app/views/new.blade.php
@@ -0,0 +1,72 @@
+@extends('templates.default.new')
+
+@section('contenido')
+	<div id="site_map">
+		<a href=""><h1 class="h1_link">Páginas </h1></a> <span class="icon-pagesuniE604"></span> <h1> Nueva Página</h1>
+	</div>
+
+	{{ Form::open(['url' => 'crawl/paginas/new']) }}
+		<section class="conf_item">
+			<h2>Nombre</h2>
+			<p>
+				<label for="nombre">¿Qué nombre tendrá tu página en el menú?</label><input type="text" name="name" autofocus placeholder="nombre">
+			</p>
+		</section>
+		<section class="conf_item">
+			<h2>Titulo</h2>
+			<p>
+				<label for="nombre">¿Cual será el titulo de tu página?</label><input type="text" name="title" placeholder="titulo">
+			</p>
+		</section>
+		<section class="conf_item">
+			<h2>Palabras Clave</h2>
+			<p>
+				<label for="nombre">¿Qué palabras definen tu sitio?</label><input type="text" name="keywords" placeholder="keywords">
+			</p>
+		</section>
+		<section class="conf_item">
+			<h2>Layouts</h2>
+			<p>
+				Elije la distribución de tu nueva página.
+			</p>
+			<div class="layout_box">
+				<div class="layout_item">
+					<p>
+						<input type="radio" name="layout" id="1" value="1" checked>
+						Uno
+					</p>
+					<img src="" alt="">
+				</div><!--
+				--><div class="layout_item">
+					<p>
+						<input type="radio" name="layout" id="2" value="2">
+						Dos
+					</p>
+					<img src="" alt="">
+				</div><!--
+				--><div class="layout_item">
+					<p>
+						<input type="radio" name="layout" id="3" value="3">
+						Tres
+					</p>
+					<img src="" alt="">
+				</div>
+			</div>
+		</section>
+		<section class="conf_item">
+			<script type="text/javascript" src="http://js.nicedit.com/nicEdit-latest.js"></script>
+			<script type="text/javascript">
+				bkLib.onDomLoaded(function() { nicEditors.allTextAreas() });
+			</script>
+
+			<h2>Contenido</h2>
+			<textarea name="content" style="width: 100%;background-color: white;height: 400px;"></textarea>
+		</section>
+		<div id="data_actions">
+			<a href=""><span class="icon-pagesclose"> Cancelar</a>
+			<a href=""><span class="icon-pagesdisk"> Guardar Cambios</a>
+			<button type="submit"><span class="icon-pagesdisk"> Guardar Cambios</button>
+		</div>
+	{{ Form::close() }}
+
+@endsection
---
+++ app/views/templates/default/new.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Este es el tamplate principal</title>
+	{{ HTML::style('css/template.css') }}
+	{{ HTML::style('css/nueva.css') }}
+	{{ HTML::script('js/jquery-1.10.2.min.js') }}
+	{{ HTML::script('js/template.js') }}
+</head>
+<body>
+	<header>
+		<div id="logo">
+			<figure>
+				{{ HTML::image('crawlCMS_logo.png','Logo') }}
+			</figure>
+		</div>
+		<div id="action">
+			<div id="account">
+				<ul>
+					<li>{{ Auth::user()->username }}</li>
+					<li>{{ HTML::link('crawl/perfil','Mi cuenta') }}</li>
+					<li>{{ HTML::link('crawl/logout', 'Salir') }}</li>
+				</ul>
+			</div>
+			<div id="user">
+				<figure>
+					{{ HTML::image('emilmdos.png','Profile Picture') }}
+				</figure>
+			</div>
+		</div>
+	</header>
+
+	<section id="main">
+		<section id="menu">
+			<nav>
+				<ul>
+					<li><a href="">Configuración</a></li>
+					<li>{{ HTML::link('crawl/paginas','Páginas') }}</li>
+					<li><a href="">Widgets</a></li>
+					<li><a href="">Usuarios</a></li>
+				</ul>
+			</nav>
+		</section>
+		<section id="content">
+			@yield('contenido')
+		</section>
+	</section>
+</body>
+</html>
