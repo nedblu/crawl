29946794dcd34cbdd35ab249c0db96d19f839793
aguilarcarlos <carlosaguilarnet@gmail.com>
2013-12-15 14:32:40 -0600

    Sitecontroller added and migration's config has been modified

--- app/controllers/SiteController.php
+++ app/controllers/SiteController.php
@@ -7,6 +7,11 @@ class SiteController extends \BaseController {
 		$nav = DB::table('pages')->where('id','!=',1)->get(['name','status']);
 		return $nav;
 	}
+	private function getFooter()
+	{
+		$footer = DB::table('config')->get();
+		return $footer;
+	}
 	private function getDataIndex()
 	{
 		$data = Page::find(1)->get(['title','keywords','content','name']);
@@ -15,7 +20,11 @@ class SiteController extends \BaseController {
 	public function index()
 	{
 		$data = Page::find(1)->get(['title','keywords','content','name']);
-		return View::make('themes.index')->with('links', self::getPages())->with('data', $data);
+		return View::make('themes.index')
+					->with('links', self::getPages())
+					->with('data', $data)
+					->with('footer', self::getFooter());
+		//return self::getFooter();
 	}
 
 	public function pages($page)
@@ -23,6 +32,11 @@ class SiteController extends \BaseController {
 		$data = Page::where('link',$page)->get(['title','keywords','content']);
 		$index = Page::find(1)->get(['name']);
 
-		return View::make('themes.pages')->with('links', self::getPages())->with('data', $data)->with('index',$index);
+		return View::make('themes.pages')
+					->with('links', self::getPages())
+					->with('data', $data)
+					->with('index',$index)
+					->with('footer', self::getFooter());
+		//return self::getFooter();
 	}
 }
